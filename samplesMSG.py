3bc3d6ad7d81157d02c4661d8c4b04b1a8ff5c95
Jordan Del Pilar <redjordan123@gmail.com>
2022-12-20 13:02:45 -0800

    New Features Added time parsing and better time handling in general Program can now see when it reaches the end of filled in rows. Hard coded row limit is still there as a saftey measure.

--- samplesMSG.py
+++ samplesMSG.py
@@ -1,20 +1,20 @@
 from openpyxl import Workbook,load_workbook
-from datetime import datetime, timedelta, time
+from datetime import datetime
 from tkinter import *
 from tkinter import filedialog
-from tkinter import simpledialog
-from time import sleep
+from parsedatetime import Calendar
 import os
 import re
 import subprocess
+
 
 class WorkOrder():
     order_number = None
     phone_number = None
     customer_name = None
-    day = None
-    time_start = None
-    time_end = None
+    delivery_day = None
+    start_time = None
+    end_time = None
     address = None
     message = None
 
@@ -114,29 +114,37 @@ Customer Number: {}
         txt_path = os.path.split(self.path)[0] + "/" + datetime.now().strftime("%m-%d-%Y") + ".txt" #Creating path to text in same directory as spreadsheet
         f = open(txt_path, "a")
 
-        """
-        Using a hard coded value of 10000 for how many rows to check.
-        Add some sort of logic to check if no further entries are on the sheet.
-        Something like if x number of rows are blank, you've reached the end of the sheet
-        """
-        i = 1
-        while i <= 10000:       
-            value = str(self.ws[("B" + str(i))].value)
+        
+        row = 1             #Set Initial Row number
+        blank_rows = 0      #Var to count number of blank rows
+        while row <= 10000:
+            value = str(self.ws[("B" + str(row))].value)
             if value.isnumeric() == True:
+                blank_rows = 0      #Reset blank row count as order was found
                 self.order.order_number = value
-                self.order.phone_number = re.sub('\D','',str(self.ws[("I" + str(i))].value))
-                self.order.customer_name = str(self.ws[("D" + str(i))].value)
-                self.get_time(i)
-                self.order.address = str(self.ws[("F" + str(i))].value)
+                self.order.phone_number = re.sub('\D','',str(self.ws[("I" + str(row))].value))
+                self.order.customer_name = str(self.ws[("D" + str(row))].value)
+                self.get_time(row)
+                self.order.address = str(self.ws[("F" + str(row))].value)
                 self.order.message = message.format(
                     self.order.order_number,
-                    self.order.day, 
-                    self.order.time_start,
-                    self.order.time_end,
+                    self.order.delivery_day, 
+                    self.order.start_time,
+                    self.order.end_time,
                     self.order.address,
                 )
                 self.write_to_text(f)
-            i = i + 1
+            else:
+                if value == "None":
+                    blank_rows = blank_rows + 1
+                else:
+                    blank_rows = 0      #Reset blank row count as value of some kind was found
+            
+            print(blank_rows)
+            if blank_rows > 4:          #if we have 4 or more blank rows in a row
+                break                   #End the loop
+            else:
+                row = row + 1           #otherwise move to the next row
 
         self.edit_entry("Done!")
         f.close()
@@ -144,54 +152,34 @@ Customer Number: {}
 
     def get_time(self, row): #Gets time from spreadsheet and calculates range. Formats time with AM/PM.
         value = str(self.ws[("H" + str(row))].value)
-        """
-        Messy try except block was added to handle incorrectly formatted times
-        I'm sure there is a cleaner way to do this but don't have one right now
-        Find cleaner solution to handle different time formats
-        """
-        try:
-            start_time = datetime.strptime(value, '%m/%d/%Y %I:%M %p')
-            print(start_time)
-        except:
-            try:
-                start_time = datetime.strptime(value, '%Y-%m-%d %I:%M:%S')
-            except:
-                #prompt user to read string and enter number of hours
-                hours = simpledialog.askinteger(title = "Hour Check", prompt = "Enter number of Hours in following time\n%s" % value)
-                start_time = time(hours, 0, 0)
-                if "PM" in value:
-                    start_time = start_time + timedelta(hours = 12)
-        start_time = int(start_time.hour) + 1
-        print(start_time)
-        if "AM" in value:
-            print("AM")
-            if start_time <= 3:     #Early Morning deliveries are all set to 4am
-                start_time = 4
-        self.order.time_start = start_time
-        self.order.time_end = start_time + 2
+        cal = Calendar()      #Create calendar object so we can parse time
+        time = cal.parse(value)             #Convert human readable time to timedate object
+        start_time = time[0].tm_hour        #Set start_time to hour found before
+        if start_time <= 3:     #Early Morning deliveries are all set to 4am
+            start_time = 4
+        self.order.start_time = start_time
+        self.order.end_time = start_time + 2
 
-        if self.order.time_start > 12:
-            self.order.time_start = str(self.order.time_start - 12)
-            print(self.order.time_start)
-            self.order.time_start = self.order.time_start + ":00 PM"
-        elif self.order.time_start == 12:
-            self.order.time_start = str(self.order.time_start) + ":00 PM"
+        if self.order.start_time > 12:
+            self.order.start_time = str(self.order.start_time - 12)
+            self.order.start_time = self.order.start_time + ":00 PM"
+        elif self.order.start_time == 12:
+            self.order.start_time = str(self.order.start_time) + ":00 PM"
         else:
-            self.order.time_start = str(self.order.time_start) + ":00 AM"
+            self.order.start_time = str(self.order.start_time) + ":00 AM"
 
-        if self.order.time_end > 12:
-            self.order.time_end = str(self.order.time_end - 12)
-            self.order.time_end = self.order.time_end + ":00 PM"
-        elif self.order.time_end == 12:
-            self.order.time_end = str(self.order.time_end) + ":00 PM"
+        if self.order.end_time > 12:
+            self.order.end_time = str(self.order.end_time - 12)
+            self.order.end_time = self.order.end_time + ":00 PM"
+        elif self.order.end_time == 12:
+            self.order.end_time = str(self.order.end_time) + ":00 PM"
         else:
-            self.order.time_end = str(self.order.time_end) + ":00 AM"
+            self.order.end_time = str(self.order.end_time) + ":00 AM"
 
         if datetime.today().weekday() == 4:     #Check if sending day is Friday. If it is change the message to say Monday delivery day
-            self.order.day = "Monday"
+            self.order.delivery_day = "Monday"
         else:
-            self.order.day = "tomorrow"
-
+            self.order.delivery_day = "tomorrow"
 
 if __name__ == "__main__":
     root = Tk()
