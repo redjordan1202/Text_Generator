d6d709aa9eaea144637ffbf9c3346137bdf910d6
Jordan Del Pilar <redjordan123@gmail.com>
2022-12-20 10:20:46 -0800

    Inital Refactor Commit. Clearing out old files and starting with a clean directory

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Python\samplesMSG
+set VIRTUAL_ENV=C:\Users\jordan.delpilar\OneDrive - Turf Distributors\Documents\Python\samplesMSG\samplesMSG
 
 if not defined PROMPT set PROMPT=$P$G
 
