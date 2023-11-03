64be4d2dfe3cd55f68b56b4bf57fca03e050f865
Jordan <redjordan123@gmail.com>
2023-11-03 09:07:48 -0700

    bug fixes and error handling
    Fix bug handling dates pulled from the load sheet.
    Added additional error handling that allows the program to contuine if an error occurs.

--- text_generator.py
+++ text_generator.py
@@ -42,6 +42,7 @@ You will receive another text notification 30 minutes prior to arrival. If there
 
 
 def main():
+    has_error = False
     print(f"{text_colors['green']}Turf Distributors ETA Text Message Generator")
     print("Written by Jordan Del Pilar")
     print(f"jordan.delpilar@turfdistribors.com{text_colors['endc']}")
@@ -75,7 +76,7 @@ def main():
     get_columns(ws, HEADER_ROW)
 
     while True:
-        choice = input("Press S to start processing\nOr press E to exit")
+        choice = input("Press S to start processing\nOr press E to exit:    ")
         choice = choice.upper()
         match choice:
             case "S":
@@ -100,6 +101,7 @@ def main():
                 order.customer_name = ws[columns["customer_name"] + str(i)].value
                 order.address = ws[columns["address"] + str(i)].value
 
+                # Process and format phone number
                 phone_number = re.sub('\D', '', str(ws[columns["phone_number"] + str(i)].value))
                 if phone_number:
                     if phone_number[0] == 1:
@@ -110,9 +112,27 @@ def main():
                         phone_number = phone_number[:12]
                 order.phone_number = phone_number
 
+                # Parse start time
                 raw_time = ws[columns["datetime"] + str(i)].value
-                cal = Calendar()
-                time = cal.parse(raw_time)
+                if type(raw_time) == datetime:
+                    try:
+                        raw_time = raw_time.strftime("%m/%d/%Y %H:%M:%S %p")
+                    except:
+                        try:
+                            raw_time = raw_time.strftime("%m/%d/%Y %H:%M %p")
+                        except:
+                            print(f"{text_colors['red']}ERROR{text_colors['endc']} - Failed to convert date to string")
+                            has_error = True
+                            continue
+                try:
+                    cal = Calendar()
+                    time = cal.parse(raw_time)
+                except TypeError:
+                    print(f"{text_colors['red']}ERROR{text_colors['endc']} - Failed to parse date")
+                    has_error = True
+                    continue
+
+                # Set start time and convert from 24hr time to 12hr time
                 start_time = time[0].tm_hour
                 if start_time <= 3:
                     start_time = 4
@@ -132,6 +152,7 @@ def main():
                 else:
                     order.end_time = str(end_time) + ":00AM"
 
+                # Check if tomorrow is a weekday or not
                 if datetime.today().weekday() == 4:
                     order.delivery_day = "Monday"
                 else:
@@ -146,7 +167,8 @@ def main():
                 )
 
                 print("Writing to file\n")
-                f.write("""Customer Name: %s
+                try:
+                    f.write("""Customer Name: %s
 Customer Phone Number: %s
 WO Number: %s
 
@@ -158,18 +180,32 @@ WO Number: %s
                     order.order_number,
                     order.message
                 ))
+                except:
+                    print(f"{text_colors['red']}ERROR{text_colors['endc']} - Failed to write to file.")
+                    has_error = True
+                    continue
                 wo_processed = wo_processed + 1
+
         i = i + 1
 
     f.close()
-    subprocess.Popen(["notepad.exe", txt_path])
+    try:
+        subprocess.Popen(["notepad.exe", txt_path])
+    except:
+        print(f"{text_colors['red']}ERROR{text_colors['endc']} - Failed to open notepad. Please open the file directly")
+        has_error = True
 
     print(f"{text_colors['green']}")
     print("=" * 80)
     print("Processing Finished!")
+    if has_error:
+        print(f"{text_colors['red']}\n!!! Warning !!!{text_colors['endc']}") 
+        print("There were some errors encounted during processing. Some Orders may not have been processed.")
+        print("Please verify which orders were processed and report any errors displayed to Jordan\n")
+        print(f"{text_colors['green']}")
+        print("=" * 80)
     print(f"{wo_processed} Work Orders Processed")
-    input("Press Enter to exit")
-    print(f"{text_colors['endc']}")
+    input(f"{text_colors['endc']}Press Enter to exit    ")
 
 
 def get_columns(ws, header_row):
